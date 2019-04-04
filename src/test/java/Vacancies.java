336498b1e64399cf91f12ccf361f21000ec0cd76
Lucifer-is-my-pet <dasha1994@list.ru>
2019-04-05 00:07:29 +0500

    бегущие по браузерным фабрикам

---
+++ src/test/java/Vacancies.java
@@ -0,0 +1,61 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.*;
+
+public class Vacancies extends BaseRunner {
+
+    @Test
+    public void testVacanciesErrors() {
+        driver.get(baseUrl);
+        driver.findElement(By.name("name")).click();
+        driver.findElement(By.name("birthday")).click();
+        driver.findElement(By.name("city")).click();
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("phone")).click();
+        driver.findElement(By.name("socialLink0")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Добавить ещё ссылку'])[1]/following::div[6]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='условиями передачи информации'])[1]/following::span[1]")).click();
+        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Фамилия и имя'])[1]/following::div[2]")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Дата рождения'])[1]/following::div[3]")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Город проживания'])[1]/following::div[3]")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Электронная почта'])[1]/following::div[2]")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Мобильный телефон'])[1]/following::div[2]")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='загрузите резюме/портфолио'])[1]/following::div[1]")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='условиями передачи информации'])[1]/following::div[1]")).getText());
+    }
+
+    @Test
+    public void testVacanciesInvalid() {
+        driver.get(baseUrl);
+        driver.findElement(By.name("name")).click();
+        driver.findElement(By.name("name")).clear();
+        driver.findElement(By.name("name")).sendKeys("!!!");
+        driver.findElement(By.name("name")).click();
+        driver.findElement(By.name("birthday")).click();
+        driver.findElement(By.name("birthday")).clear();
+        driver.findElement(By.name("birthday")).sendKeys("00.00.0000");
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).clear();
+        driver.findElement(By.name("email")).sendKeys("111");
+        driver.findElement(By.name("phone")).click();
+        driver.findElement(By.name("phone")).clear();
+        driver.findElement(By.name("phone")).sendKeys("(000) 000-00-00");
+        driver.findElement(By.name("socialLink0")).click();
+        driver.findElement(By.name("socialLink0")).clear();
+        driver.findElement(By.name("socialLink0")).sendKeys("text");
+        assertEquals("Код города/оператора должен начинаться с цифры 3, 4, 5, 6, 8, 9", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Мобильный телефон'])[1]/following::div[2]")).getText());
+        // ERROR: Caught exception [ERROR: Unsupported command [setText | name=phone | ]]
+        driver.findElement(By.name("phone")).clear();
+        driver.findElement(By.name("phone")).sendKeys("(333)");
+        driver.findElement(By.name("socialLink0")).click();
+        driver.findElement(By.name("socialLink0")).clear();
+        driver.findElement(By.name("socialLink0")).sendKeys("new text");
+        assertEquals("Допустимо использовать только буквы русского алфавита и дефис", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Фамилия и имя'])[1]/following::div[2]")).getText());
+        assertEquals("Поле заполнено некорректно", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Дата рождения'])[1]/following::div[3]")).getText());
+        assertEquals("Введите корректный адрес эл. почты", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Электронная почта'])[1]/following::div[2]")).getText());
+        assertEquals("Номер телефона должен состоять из 10 цифр, начиная с кода оператора", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Мобильный телефон'])[1]/following::div[2]")).getText());
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Перетащите файлы сюда'])[1]/following::span[2]")).click();
+    }
+}
