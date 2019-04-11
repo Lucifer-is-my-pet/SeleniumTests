f11dd94fa890b33bc2bd14036852c7f03207e1d0
Lucifer-is-my-pet <dasha1994@list.ru>
2019-04-12 00:17:13 +0500

    пункт 2 и 2.1 дз

--- src/test/java/Vacancies.java
+++ src/test/java/Vacancies.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.*;
 
 import org.openqa.selenium.*;
@@ -59,11 +60,17 @@ public class Vacancies extends BaseRunner {
         driver.findElement(By.name("socialLink0")).click();
         driver.findElement(By.name("socialLink0")).clear();
         driver.findElement(By.name("socialLink0")).sendKeys("text");
+
         assertEquals("Номер телефона должен состоять из 10 цифр, начиная с кода оператора",
-                driver.findElement(By.xpath("(//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')])[5]")).getText());
+                driver.findElement(By.xpath("(//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')])[4]")).getText());
+        assertEquals("Допустимо использовать только буквы русского алфавита и дефис",
+                driver.findElement(By.xpath("//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')][1]")).getText());
+        assertEquals("Поле заполнено некорректно",
+                driver.findElement(By.xpath("(//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')])[2]")).getText());
+        assertEquals("Введите корректный адрес эл. почты",
+                driver.findElement(By.xpath("(//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')])[3]")).getText());
 
         driver.findElement(By.name("phone")).click();
-        driver.findElement(By.name("phone")).sendKeys(Keys.BACK_SPACE);
         driver.findElement(By.name("phone")).sendKeys(Keys.BACK_SPACE);
         driver.findElement(By.name("phone")).sendKeys(Keys.BACK_SPACE);
         driver.findElement(By.name("phone")).sendKeys(Keys.BACK_SPACE);
@@ -73,33 +80,45 @@ public class Vacancies extends BaseRunner {
 
         driver.findElement(By.xpath("//button[@aria-label='Отправить']")).click();
 
-        assertEquals("Допустимо использовать только буквы русского алфавита и дефис",
-                driver.findElement(By.xpath("//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')][1]")).getText());
-        assertEquals("Поле заполнено некорректно",
-                driver.findElement(By.xpath("(//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')])[2]")).getText());
-        assertEquals("Введите корректный адрес эл. почты",
-                driver.findElement(By.xpath("(//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')])[4]")).getText());
         assertEquals("Код города/оператора должен начинаться с цифры 3, 4, 5, 6, 8, 9",
                 driver.findElement(By.xpath("(//div[contains(@class, 'ui-form-field-error-message') and contains(@class, 'ui-form-field-error-message_ui-form')])[5]")).getText());
     }
 
     @Test
     public void switchTabs() {
-        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "t");
-        driver.get("https://www.google.ru/");
-        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles()); // ???
+        driver.get(baseUrl);
+//        driver.findElement(By.cssSelector("body")).sendKeys(Keys.chord(Keys.CONTROL, "t")); // не работает
+//        driver.get("https://www.google.ru/");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://www.google.ru/','_blank');");
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
-        driver.close();
-        driver.switchTo().window(tabs.get(0));
-
-/*        //Open a new tab using Ctrl + t
-        //Switch between tabs using Ctrl + \t
-        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL +"\t");*/
 
         driver.findElement(By.name("q")).sendKeys("мобайл тинькофф n");
-//        Select dropdown = new Select(driver.findElement(By.id("identifier")));
         driver.findElement(By.xpath("//*[contains(text(), 'тарифы')]/parent::span")).click();
-//        driver.findElement(By.xpath("//a[contains(@href,'listDetails.do')]")).click();
         driver.findElement(By.xpath("//cite[text()='https://www.tinkoff.ru/mobile-operator/tariffs/']")).click();
+
+        tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(2));
+        assertEquals("Тарифы Тинькофф Мобайла", driver.getTitle());
+
+        driver.switchTo().window(tabs.get(1));
+        driver.close();
+
+        driver.switchTo().window(tabs.get(2));
+        assertEquals("https://www.tinkoff.ru/mobile-operator/tariffs/", driver.getCurrentUrl());
+    }
+
+    @Test
+    public void changeRegion() throws InterruptedException {
+        ((JavascriptExecutor) driver).executeScript("window.open('https://www.tinkoff.ru/mobile-operator/tariffs/','_blank');");
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+
+        driver.findElement(By.xpath("//div[@class='MvnoRegionConfirmation__title_DOqnW']")).click();
+        driver.findElement(By.xpath("//div[contains(text(), 'Москва и Московская обл.')]")).click();
+        assertEquals("Москва и Московская обл.", driver.findElement(By.xpath("//div[@class='MvnoRegionConfirmation__title_DOqnW']")).getText());
+        sleep(3000); // страница перезагружается
+        assertEquals("Москва и Московская обл.", driver.findElement(By.xpath("//div[@class='MvnoRegionConfirmation__title_DOqnW']")).getText());
+
     }
 }
